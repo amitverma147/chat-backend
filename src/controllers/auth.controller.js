bebba469b2d8b282b2fe246b2a500593ed1144b1
Amit Verma <vamit20000@gmail.com>
2025-07-11 12:52:58 +0530

    signup functionality done

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,74 @@
+import User from "../models/User.model.js";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res) => {
+  const { email, password, fullName } = req.body;
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be at least 6 characters",
+      });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const exisitingUser = await User.findOne({ email });
+
+    if (exisitingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "Email already exists, please use a different one.",
+      });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1;
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+    const newUser = await User.create({
+      email,
+      password,
+      fullName,
+      profilepic: randomAvatar,
+    });
+
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(201).json({ success: true, user: newUser });
+  } catch (error) {
+    console.log("Error in signUp controller:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+
+export const signin = async (req, res) => {
+  res.send("Signin Route");
+};
+
+
+export const logout = (req, res) => {
+  res.send("logout Route");
+};
---
+++ src/models/User.model.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [6, "Minimum 6 length of password"],
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    profilepic: {
+      type: String,
+      default: "",
+    },
+    nativeLanguage: {
+      type: String,
+      default: "",
+    },
+    learningLanguage: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    inOnboarded: {
+      type: Boolean,
+      default: false,
+    },
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+//prehook
+//can not use arrow function : pre-hook, which doesn't bind this to the document instance.
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+const User = mongoose.model("User", userSchema);
+
+export default User;
