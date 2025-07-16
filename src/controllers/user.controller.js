18246540cd3c405935aabe1b56db629c3022d8ce
Amit Verma <vamit20000@gmail.com>
2025-07-16 15:10:39 +0530

    controller fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,41 +1,42 @@
-import FriendRequest from "../models/FriendRequest.model.js";
 import User from "../models/User.model.js";
+import FriendRequest from "../models/FriendRequest.model.js";
 
-export const getRecommendedUsers = async (req, res) => {
+export async function getRecommendedUsers(req, res) {
   try {
     const currentUserId = req.user.id;
     const currentUser = req.user;
 
     const recommendedUsers = await User.find({
       $and: [
-        { _id: { $ne: currentUserId } },
-        { $id: { $in: currentUser.friends } },
+        { _id: { $ne: currentUserId } }, //exclude current user
+        { _id: { $nin: currentUser.friends } }, // exclude current user's friends
         { inOnboarded: true },
       ],
     });
     res.status(200).json(recommendedUsers);
   } catch (error) {
-    console.error("Error in getRecommendedUsers Controller", error.message);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
+    console.error("Error in getRecommendedUsers controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-};
+}
 
-export const getMyFriends = async (req, res) => {
+export async function getMyFriends(req, res) {
   try {
     const user = await User.findById(req.user.id)
       .select("friends")
       .populate(
         "friends",
-        "fullName profilepic nativeLanguage learningLanguage"
+        "fullName profilePic nativeLanguage learningLanguage"
       );
+
     res.status(200).json(user.friends);
   } catch (error) {
     console.error("Error in getMyFriends controller", error.message);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
-};
+}
 
-export const sendFriendRequest = async (req, res) => {
+export async function sendFriendRequest(req, res) {
   try {
     const myId = req.user.id;
     const { id: recipientId } = req.params;
@@ -48,17 +49,18 @@ export const sendFriendRequest = async (req, res) => {
     }
 
     const recipient = await User.findById(recipientId);
-
     if (!recipient) {
       return res.status(404).json({ message: "Recipient not found" });
     }
 
+    // check if user is already friends
     if (recipient.friends.includes(myId)) {
       return res
         .status(400)
-        .json({ message: "You are already Friends with this user" });
+        .json({ message: "You are already friends with this user" });
     }
 
+    // check if a req already exists
     const existingRequest = await FriendRequest.findOne({
       $or: [
         { sender: myId, recipient: recipientId },
@@ -67,9 +69,11 @@ export const sendFriendRequest = async (req, res) => {
     });
 
     if (existingRequest) {
-      return res.status(400).json({
-        message: "A friend request already exists between you adn this user",
-      });
+      return res
+        .status(400)
+        .json({
+          message: "A friend request already exists between you and this user",
+        });
     }
 
     const friendRequest = await FriendRequest.create({
@@ -79,12 +83,12 @@ export const sendFriendRequest = async (req, res) => {
 
     res.status(201).json(friendRequest);
   } catch (error) {
-    console.error("Error in sendFriendRequest Controller:", error.message);
+    console.error("Error in sendFriendRequest controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
-};
+}
 
-export const acceptFriendRequest = async (req, res) => {
+export async function acceptFriendRequest(req, res) {
   try {
     const { id: requestId } = req.params;
 
@@ -119,7 +123,7 @@ export const acceptFriendRequest = async (req, res) => {
     console.log("Error in acceptFriendRequest controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
-};
+}
 
 export async function getFriendRequests(req, res) {
   try {
